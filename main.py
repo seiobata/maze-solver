620039eac50567afe605062b8be0352c9377144e
seiobata <s.bautista18@yahoo.com>
2025-03-26 10:55:40 -0500

    Add Point and Line and draw methods

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from window import Window
+from window import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+
+    point1 = Point(100, 100)
+    point2 = Point(100, 200)
+    point3 = Point(200,200)
+    line1 = Line(point1, point2)
+    line2 = Line(point2, point3)
+    line3 = Line(point1, point3)
+    for line in [line1, line2, line3]:
+        win.draw_line(line, "black")
+
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -1,23 +1,44 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window():
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.canvas = Canvas(self.__root, bg="silver", width=width, height=height)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.is_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close())
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__is_running = False
 
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
+        self.__is_running = True
+        while self.__is_running:
             self.redraw()
 
     def close(self):
-        self.is_running = False
+        self.__is_running = False
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, 
+            fill=fill_color, width=2,
+        )
